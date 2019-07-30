446bea619c139637135c188eda7b6dbfd3e37c07
Muttakin Islam <islammuttakin@live.com>
2019-07-30 18:33:39 +0600

    views

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Simple-chat-app
+Modules needed
+1) Express
+2) Mongoose
+3) ejs
+4) Nodemon
+5) socket.io
--- Server.js
+++ Server.js
@@ -1,11 +1,10 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
 
-
 app.get('/', function(request, response){
-  response.render('index.ejs');
+  response.render('landingpage.ejs');
 });
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
