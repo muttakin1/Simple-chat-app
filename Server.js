17f4f14d4a1902479d8cd95a5a5d7a87b6ff3f3f
Muttakin Islam <islammuttakin@live.com>
2019-08-09 11:20:18 +0600

    front end

--- Server.js
+++ Server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var path = require ('path');
 var app = require('express')();
 var http = require('http').createServer(app);
+const CURRENT_WORKING_DIR = process.cwd()
 
 app.get('/', function(request, response){
   response.render('landingpage.ejs');
 });
+app.use(express.static(path.join(__dirname, 'static')));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
